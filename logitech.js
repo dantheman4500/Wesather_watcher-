392da428d68483c0d235336d679c09d81002d51f
Daniele <danielromero4500@gmail.com>
2022-09-29 17:29:11 -0700

    searchbar JS

--- logitech.js
+++ logitech.js
@@ -1,4 +1,17 @@
 var apiKey = "3850992ef985a31e47bda004979cefeb";
-
 var apiUrl = 'https://api.openweathermap.org';
+var search = "";
+var searchHistory = [];
 
+var searchBar = document.getElementById("searchInput");
+var searchBtn = document.getElementById("submit");
+
+//--------- Search bard -------------------------// 
+
+object.addEventListener("click", (getcity) =>{});
+
+getcity => {
+    search = searchBar.value
+}
+
+console.log(search)
